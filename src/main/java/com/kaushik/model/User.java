9c77998cb075f976f6517b10e103f9152f500564
Kaushikj14 <kaushikjain1423@gmail.com>
2025-03-18 20:39:00 +0530

    Added the dependency and made some changes .

--- src/main/java/com/kaushik/model/User.java
+++ src/main/java/com/kaushik/model/User.java
@@ -6,12 +6,20 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.NoArgsConstructor;
 import org.hibernate.annotations.CreationTimestamp;
 import org.hibernate.annotations.UpdateTimestamp;
 
 import java.time.LocalDateTime;
 
+
 @Entity
+
+//Added the lombok dependency
+@AllArgsConstructor
+@NoArgsConstructor
+
 public class User {
 
     @jakarta.persistence.Id
@@ -41,92 +49,92 @@ public class User {
     private LocalDateTime updatedAt;
 
 
-
-    public User() {
-    }
-
-    public User(Long id, String fullName, String userName, String email, String phone, String password, String role, LocalDateTime createdAt, LocalDateTime updatedAt) {
-        Id = id;
-        this.fullName = fullName;
-        this.userName = userName;
-        this.email = email;
-        this.phone = phone;
-        this.password = password;
-        this.role = role;
-        this.createdAt = createdAt;
-        this.updatedAt = updatedAt;
-    }
-
-    public @NotBlank(message = "userName is required") String getUserName() {
-        return userName;
-    }
-
-
-    public void setUserName(@NotBlank(message = "userName is required") String userName) {
-        this.userName = userName;
-    }
-
-    public String getPassword() {
-        return password;
-    }
-
-    public void setPassword(String password) {
-        this.password = password;
-    }
-
-    public Long getId() {
-        return Id;
-    }
-
-    public void setId(Long id) {
-        Id = id;
-    }
-
-    public String getFullName() {
-        return fullName;
-    }
-
-    public void setFullName(String fullName) {
-        this.fullName = fullName;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public String getPhone() {
-        return phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public String getRole() {
-        return role;
-    }
-
-    public void setRole(String role) {
-        this.role = role;
-    }
-
-    public LocalDateTime getCreatedAt() {
-        return createdAt;
-    }
-
-    public void setCreatedAt(LocalDateTime createdAt) {
-        this.createdAt = createdAt;
-    }
-
-    public LocalDateTime getUpdatedAt() {
-        return updatedAt;
-    }
-
-    public void setUpdatedAt(LocalDateTime updatedAt) {
-        this.updatedAt = updatedAt;
-    }
+//
+//    public User() {
+//    }
+//
+//    public User(Long id, String fullName, String userName, String email, String phone, String password, String role, LocalDateTime createdAt, LocalDateTime updatedAt) {
+//        Id = id;
+//        this.fullName = fullName;
+//        this.userName = userName;
+//        this.email = email;
+//        this.phone = phone;
+//        this.password = password;
+//        this.role = role;
+//        this.createdAt = createdAt;
+//        this.updatedAt = updatedAt;
+//    }
+//
+//    public @NotBlank(message = "userName is required") String getUserName() {
+//        return userName;
+//    }
+//
+//
+//    public void setUserName(@NotBlank(message = "userName is required") String userName) {
+//        this.userName = userName;
+//    }
+//
+//    public String getPassword() {
+//        return password;
+//    }
+//
+//    public void setPassword(String password) {
+//        this.password = password;
+//    }
+//
+//    public Long getId() {
+//        return Id;
+//    }
+//
+//    public void setId(Long id) {
+//        Id = id;
+//    }
+//
+//    public String getFullName() {
+//        return fullName;
+//    }
+//
+//    public void setFullName(String fullName) {
+//        this.fullName = fullName;
+//    }
+//
+//    public String getEmail() {
+//        return email;
+//    }
+//
+//    public void setEmail(String email) {
+//        this.email = email;
+//    }
+//
+//    public String getPhone() {
+//        return phone;
+//    }
+//
+//    public void setPhone(String phone) {
+//        this.phone = phone;
+//    }
+//
+//    public String getRole() {
+//        return role;
+//    }
+//
+//    public void setRole(String role) {
+//        this.role = role;
+//    }
+//
+//    public LocalDateTime getCreatedAt() {
+//        return createdAt;
+//    }
+//
+//    public void setCreatedAt(LocalDateTime createdAt) {
+//        this.createdAt = createdAt;
+//    }
+//
+//    public LocalDateTime getUpdatedAt() {
+//        return updatedAt;
+//    }
+//
+//    public void setUpdatedAt(LocalDateTime updatedAt) {
+//        this.updatedAt = updatedAt;
+//    }
 }
